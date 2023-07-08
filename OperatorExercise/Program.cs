6cdd118cd20acabf87abee8a82214979a298b5b0
tyler davis <tyler.davis432@gmail.com>
2023-07-07 22:16:00 -0400

    operator exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -8,7 +8,27 @@
             int b = 4;
             int quotient = a / b;
             int remainder = a % b;
-            Console.WriteLine(a + "/" + b + "is" + quotient + "remainder" + remainder);
+            Console.WriteLine(a + " / " + b + " is " + quotient + " remainder " + remainder);
+            
+            Console.WriteLine("give me the radius of your circle.");
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput !);
+            
+            Console.WriteLine(AreaOfCircle(radius));
+            
+
+
         }
+        public static double AreaOfCircle(double radius)
+        {
+         var area = Math.PI * Math.Pow(radius, 2);
+
+
+            return area;
+
+        } 
+    
+
     }
 }
